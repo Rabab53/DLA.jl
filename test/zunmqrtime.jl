24f0c914f1bec27f68d687cbc9d0ed3b9f693757
rabab53 <rabab.omairy@kaust.edu.sa>
2024-07-13 16:59:57 +0300

    gpu results

--- test/zunmqrtime.jl
+++ test/zunmqrtime.jl
@@ -13,13 +13,13 @@ include("../src/zunmqrwrap.jl")
 BLAS.set_num_threads(Threads.nthreads())
 t = 40
 
-for T in [Float64, Float32, ComplexF64, ComplexF32]
+for T in [Float64, Float32] #[Float16]#[Float64, Float32, ComplexF64, ComplexF32]#[Float64]#[ComplexF32] #[Float64, Float32, ComplexF64, ComplexF32]
     println(T)
     
     side = 'L'
 
-    for ib in [64,128]
-        for trans in ['C', 'N']
+    for ib in [128]
+        for trans in ['N', 'C']
             println("ib is ", ib, " trans is ", trans)
 
             xvals = Float64[]
@@ -43,7 +43,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
             rlvj1 = Float64[]
             rjvl1 = Float64[]
             
-            for m in [16384, 32768]#[512, 1024, 2048, 4096, 8192, 16384, 32768]
+            for m in [512, 1024, 2048, 4096, 8192, 16384]
                 println("m = ", m)
                 n = m
                 k = m
@@ -78,7 +78,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
                 A3 = deepcopy(A1)
                 
                 C4 = deepcopy(C)
-                
+
                if (T == Float64 || T == Float32) && trans == 'C'
                     #LAPACK
                     s = @belapsed  unmqr!($T, $side, 'T', $A1, $Tau1, $C0) samples = 7 evals = 1
@@ -108,13 +108,17 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
                     push!(y,s)
                     push!(ym, sm / 10^3)
 
-
+                    
                     A1 = CuArray(A1)
                     Tau1 = CuArray(Tau1)
                     C0 = CuArray(C0)
                     #CU
-                    s1 = @belapsed  CUSOLVER.ormqr!($side, $trans, $A1, $Tau1, $C0) samples = 7 evals = 1
-                    sm1 = @ballocated  CUSOLVER.ormqr!($side, $trans, $A1, $Tau1, $C0) samples = 7 evals = 1
+                    s1 = @belapsed  @sync begin
+                        CUSOLVER.ormqr!($side, $trans, $A1, $Tau1, $C0)
+                    end
+                    sm1 = @ballocated  @sync begin
+                        CUSOLVER.ormqr!($side, $trans, $A1, $Tau1, $C0) 
+                    end
                     @show s1, sm1
                     push!(y1,s1)
                     push!(ym1, sm1 / 10^3)
@@ -122,7 +126,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
                     CUDA.unsafe_free!(Tau1)
                     CUDA.unsafe_free!(C0)
                 end
- 
+              
 
                 #Julia multithreaded
                 s0j = @belapsed zunmqrv0($side, $trans, $m, $n, $k, $ib, $A, $lda, $Tau, $ib, $C, $m, $work, $ldw) samples = 7 evals = 1
@@ -130,15 +134,19 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
                 @show s0j, s0jm
                 push!(y0j, s0j)
                 push!(y0jm, s0jm/10^3)
-
+  
                 
                 A = CuArray(A)
                 Tau = CuArray(Tau)
                 C = CuArray(C)
                 work = CuArray(work)
                 #Julia cuda 
-                s1j = @belapsed zunmqrv0g($side, $trans, $m, $n, $k, $ib, $A, $lda, $Tau, $ib, $C, $m, $work, $ldw) samples = 7 evals = 1
-                s1jm = @ballocated zunmqrv0g($side, $trans, $m, $n, $k, $ib, $A, $lda, $Tau, $ib, $C, $m, $work, $ldw) samples = 7 evals = 1
+                s1j =  @belapsed @sync begin
+                    zunmqrv0g($side, $trans, $m, $n, $k, $ib, $A, $lda, $Tau, $ib, $C, $m, $work, $ldw) 
+                end
+                s1jm = @ballocated @sync begin
+                    zunmqrv0g($side, $trans, $m, $n, $k, $ib, $A, $lda, $Tau, $ib, $C, $m, $work, $ldw) 
+                end
                 @show s1j, s1jm
                 push!(y1j, s1j)
                 push!(y1jm, s1jm/10^3)
@@ -153,6 +161,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
                 push!(rlvj1, s1/s1j)
                 push!(rjvl1, s1j/s1)
                 CUDA.reclaim()
+
             end
 
             xvals = Int.(xvals)
@@ -161,7 +170,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
             @save "unmqr type=$T t=$t ib=$ib trans=$trans.jdl2" xvals y ym  y1 ym1 y0j y0jm y1j y1jm rlvj rjvl rlvj1 rjvl1
 
             p = plot(yaxis=:log)
-            plot!(p, legend=:topleft, xlabel="Matrix Size (n x n)", ylabel="Time (s)", title="unmqr Time Multi-Thread")
+            plot!(p, legend=:topleft, xlabel="Matrix Size (n x n)", ylabel="Time (s)")
             plot!(p, xvals, y, marker=:circle, label="LAPACK")
             plot!(p, xvals, y0j, marker=:star8, label="Julia Multithreaded")
             plot!(p, xvals, y1, marker=:circle, label="CUSOLVER")
@@ -169,7 +178,7 @@ for T in [Float64, Float32, ComplexF64, ComplexF32]
             savefig(p, "unmqr time type=$T t=$t ib=$ib trans=$trans")
 
             q = plot(yaxis=:log)
-            plot!(q, legend=:topleft, xlabel="Matrix Size (n x n)", ylabel = "Memory (KB)", title="unmqr Memory Multi-Thread")
+            plot!(q, legend=:topleft, xlabel="Matrix Size (n x n)", ylabel = "Memory (KB)")
             plot!(q, xvals, ym, marker=:circle, label="LAPACK")
             plot!(q, xvals, y0jm, marker=:star8, label="Julia Multithreaded")
             plot!(q, xvals, ym1, marker=:circle, label="CUSOLVER")
