527a2150212c9b8be8e0196e1f787c4a91f6a4db
Vicki Carrica <vickicar@mit.edu>
2024-09-17 22:02:59 -0400

    Add initial file, zlauum.jl

---
+++ src/zlauum.jl
@@ -0,0 +1,4 @@
+# zlauum.jl
+# This file will contain the implementation of the ZLAUUM function
+# which performs the matrix multiplication U * U' or L' * L,
+# where U or L is stored in the upper or lower triangular part of A.
